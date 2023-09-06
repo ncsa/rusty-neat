5127d92706ada55f3856a8e35bf2c14745b01ee2
Joshua Allen <jallen17@illinois.edu>
2023-09-05 22:34:44 -0500

    Added function to select a random integer from a list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-fn 
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+fn find_random_non_n(positions: Vec<u64>) -> u64 {
+    let mut rng = thread_rng();
+    let selected = positions.choose(&mut rng);
+    *selected.unwrap()
+}
 
 fn main() {
-    println!("Hello, world!");
+    let my_positions: Vec<u64> = vec![0, 1, 2, 3, 4, 5, 6, 7];
+    let a_position: u64 = find_random_non_n(my_positions);
+    println!("Insert at {:?}", a_position);
 }

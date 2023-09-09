04753fe510739f17af7035a6892834df43a49206
Joshua Allen <jallen17@illinois.edu>
2023-09-09 12:40:51 -0500

    Adding some rust code!

---
+++ src/logger.rs
@@ -0,0 +1,27 @@
+pub use log::*;
+pub extern crate simplelog;
+
+pub use simplelog::{SimpleLogger, WriteLogger, LevelFilter, CombinedLogger, TerminalMode, Config, ColorChoice};
+
+pub use std::fs::File;
+
+pub fn init() {
+    CombinedLogger::init(
+        vec![
+            #[cfg(feature = "termcolor")]
+            TermLogger::new(
+                LevelFilter::Warn,
+                Config::default(),
+                TerminalMode::Mixed,
+                ColorChoice::Auto,
+            ),
+            #[cfg(not(feature="termolor"))]
+            SimpleLogger::new(LevelFilter::Info, Config::default()),
+            WriteLogger::new(
+                LevelFilter::Debug,
+                Config::default(),
+                File::create("this_log.log").unwrap()
+            ),
+        ]
+    ).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,47 @@
-use rand::seq::SliceRandom;
+extern crate log;
+mod logger;
+
+use rand::prelude::IteratorRandom;
 use rand::thread_rng;
 
-fn find_random_non_n(positions: Vec<u64>) -> u64 {
+fn find_random_non_n(positions: &Vec<bool>) -> Option<usize> {
     /*
     This function is intended to find a non-n position from a vector
     An improvement is would be to use the gc-bias as a weighting vector, path them both in
     and then use weighted sample.
      */
     let mut rng = thread_rng();
-    let selected = positions.choose(&mut rng);
-    *selected.unwrap()
+    let indexes = 0..positions.len();
+    let mut index = indexes.choose_stable(&mut rng);
+    while positions[index.unwrap()] == false {
+        index = find_random_non_n(positions)
+    };
+    index
 }
 
-fn map_non_n_regions(sequence: String) -> Vec<bool> {
-    let check_vec: Vec<bool> = vec![true; sequence.len()];
+fn map_non_n_regions(sequence: &String) -> Vec<bool> {
+    let mut check_vec: Vec<bool> = vec![true; sequence.len()];
     for (index, char) in sequence.chars().enumerate() {
-        if char == "N" {
+        if char == 'N' {
             check_vec[index] = false;
         }
     }
-    
+
     check_vec
 }
 
 fn main() {
-    /*
-    Ultimately, this vector will be a list of positions to sample from
-     */
-    let my_positions: Vec<u64> = vec![0, 1, 2, 3, 4, 5, 6, 7];
-    let a_position: u64 = find_random_non_n(my_positions);
-    println!("Insert at {:?}", a_position);
+    logger::init();
+    logger::info!("Begin processing");
+    let sequence = String::from("AACATACATACACATGAGCACGCGACTACATGAGACGTAGCA");
+    logger::info!("Input sequence: {}", sequence);
+    let non_n_map = map_non_n_regions(&sequence);
+    logger::debug!("Non-n-map = {:?}", non_n_map);
+    let position = find_random_non_n(&non_n_map).unwrap();
+    logger::info!("Found a position! It is {}",position);
+    let code = &sequence.as_bytes()[position];
+    let nucleotide: char = *code as char;
+    logger::info!("Nucleotide to mutate: {:?}", nucleotide);
+    println!("End");
 }
+

23ea517fb9513a51fbf377fa64b8b100074b768c
Joshua Allen <jallen17@illinois.edu>
2023-09-19 08:26:53 -0500

    Merging with  work I was doing in the other env. Work in progress, not yet functioning.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 mod logger;
 use rusty_neat::{Config};
 use std::{env, process};
+use rand::{thread_rng};
+use rand::prelude::*;
 
 fn find_random_non_n(positions: &Vec<bool>) -> Option<usize> {
     /*
@@ -58,12 +60,24 @@ fn main() {
     if let Err(e) = rusty_neat::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    // let sequence = String::from("ACATCTACACGGAGCACGACGCACCNNNNNNccccNNNNCACTGCAGTGCATGACG");
-    // logger::info!("Input sequence: {}", sequence);
-    // let non_n_map = map_non_n_regions(&sequence);
-    // if !non_n_map.contains(&true) {
-    //     logger::info!("Input sequence contained no non-n values. Quitting.");
-    //     std::process::exit(0)
     }
+
+    let sequence = String::from("ACATCTACACGGAGCACGACGCACCNNNNNNccccNNNNCACTGCAGTGCATGACG");
+    logger::info!("Input sequence: {}", sequence);
+    let non_n_map = map_non_n_regions(&sequence);
+    if !non_n_map.contains(&true) {
+        logger::info!("Input sequence contained no non-n values. Quitting.");
+        std::process::exit(0)
+    }
+    logger::debug!("Non-n-map = {:?}", non_n_map);
+    let position = find_random_non_n(&non_n_map).unwrap();
+    logger::info!("Found a position! It is {}",position);
+    let code = &sequence.as_bytes()[position];
+    let nucleotide: char = *code as char;
+    logger::info!("Nucleotide to mutate: {:?}", nucleotide);
+    let new_nucleotide = generate_new_nucleotide(&nucleotide);
+    let result: String = sequence.chars().enumerate().map(|(i, c)| if i == position { new_nucleotide } else { c }).collect();
+    logger::info!("New sequence = {}", result);
+    logger::info!("End program");
 }
 

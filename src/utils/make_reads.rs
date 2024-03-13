ca30a86bf44b69e66583a19f499639fb84b271a5
Joshua Allen <jallen17@illinois.edu>
2024-03-13 00:45:32 -0500

    Making some changes to make room for paired-ended reads.

--- src/utils/make_reads.rs
+++ src/utils/make_reads.rs
@@ -8,10 +8,19 @@ fn cover_dataset(
     coverage: usize,
     rng: &mut ThreadRng,
 ) -> Vec<(usize, usize)> {
-    // This function selects the positions of the reads. It starts at the beginning and goes out
-    // one read length, then picks a random jump between 0 and half the read length to move
-    // And picks those coordinates for a second read. Once the tail of the read is past the end,
-    // we start over again at 0.
+    /*
+    Takes:
+        span_length: Total number of bases in the sequence
+        read_length: The length of the reads for this run
+        coverage: The coverage depth for the reads
+    Returns:
+        A vector of tuples (usize, usize), denoting the start and end positions of the reads
+
+    This function selects the positions of the reads. It starts at the beginning and goes out
+    one read length, then picks a random jump between 0 and half the read length to move
+    And picks those coordinates for a second read. Once the tail of the read is past the end,
+    we start over again at 0.
+     */
     // todo currently the reads look a little light so we may need to improve this calculation.
     let mut read_set: Vec<(usize, usize)> = vec![];
     let mut start: usize = 0;
@@ -31,34 +40,75 @@ fn cover_dataset(
     read_set
 }
 
+fn complement(nucleotide: u8) -> u8 {
+    /*
+    0 = A, 1 = C, 2 = G, 3 = T,
+    matches with the complement of each nucleotide.
+
+    Todo: make this part of a struct to standardize across the program.
+     */
+    return match nucleotide {
+        0 => 3,
+        1 => 2,
+        2 => 1,
+        3 => 0,
+        _ => 4,
+    }
+}
+
+fn reverse_complement(sequence: &Vec<u8>) -> Vec<u8> {
+    /*
+    Returns the reverse complement of a vector of u8's representing a DNA sequence.
+     */
+    let length = sequence.len();
+    let mut rev_comp = Vec::new();
+    for i in (0..length).rev() {
+        rev_comp.push(complement(sequence[i]))
+    }
+    rev_comp
+}
+
 pub fn generate_reads(
-    mutated_sequences: &Vec<Vec<u8>>,
+    mutated_sequence: &Vec<u8>,
     read_length: &usize,
     coverage: &usize,
-    rng: &mut ThreadRng,
+    paired_ended: bool,
+    mut rng: &mut ThreadRng,
 ) -> Box<HashSet<Vec<u8>>> {
-    // Infer ploidy
-    let ploidy = mutated_sequences.len();
-    // Need X/ploidy reads per ploid, but probably fewer
-    let coverage_per_ploid = coverage / ploidy + 1;
+    /*
+    Takes:
+        mutated_sequence: a vector of u8's representing the mutated sequence.
+        read_length: the length ef the reads for this run
+        coverage: the average depth of coverage for this run
+        rng: the random number generator for the run
+    Returns:
+        HashSet of vectors representing the read sequences, stored on the heap in box.
+
+    This takes a mutated sequence and produces a set of reads based on the mutated sequence. For
+    paired ended reads, this will generate a set of reads from each end, by taking the reverse
+    complement int the output
+     */
+
+    if paired_ended {
+        let rev_comp = reverse_complement(mutated_sequence);
+        todo!();
+    }
 
     // set up some defaults and storage
     let mut read_set: HashSet<Vec<u8>> = HashSet::new();
 
-    for i in 0..ploidy {
-        let seq_len = *(&mutated_sequences[i].len());
-        // Generate a vector of read positions
-        let read_positions: Vec<(usize, usize)> = cover_dataset(
-            seq_len,
-            *read_length,
-            coverage_per_ploid,
-            rng,
-        );
+    let seq_len = mutated_sequence.len();
+    // Generate a vector of read positions
+    let read_positions: Vec<(usize, usize)> = cover_dataset(
+        seq_len,
+        *read_length,
+        *coverage,
+        &mut rng,
+    );
 
-        // Generate the reads from the read positions.
-        for (start, end) in read_positions {
-            read_set.insert(mutated_sequences[i][start..end].into());
-        }
+    // Generate the reads from the read positions.
+    for (start, end) in read_positions {
+        read_set.insert(mutated_sequence[start..end].into());
     }
     // puts the reads in the heap.
     Box::new(read_set)

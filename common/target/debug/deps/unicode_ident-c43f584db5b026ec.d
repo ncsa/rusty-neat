04d4d8cec39e652607629a7efe1c43fe0a843192
Joshua Allen <jallen17@illinois.edu>
2024-07-30 13:54:40 -0500

    Adding a fasta reader based on the method in the debruijn graph code base. Just the base library for now

---
+++ common/target/debug/deps/unicode_ident-c43f584db5b026ec.d
@@ -0,0 +1,8 @@
+/home/joshfactorial/code/rusty-neat/common/target/debug/deps/libunicode_ident-c43f584db5b026ec.rmeta: /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/joshfactorial/code/rusty-neat/common/target/debug/deps/libunicode_ident-c43f584db5b026ec.rlib: /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/joshfactorial/code/rusty-neat/common/target/debug/deps/unicode_ident-c43f584db5b026ec.d: /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs:
+/home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs:

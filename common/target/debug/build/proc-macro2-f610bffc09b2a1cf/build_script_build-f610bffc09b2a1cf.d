f03fa48214ab106d07618a3a6c64376bcfd15377
Joshua Allen <jallen17@illinois.edu>
2024-07-30 13:54:40 -0500

    Adding a fasta reader based on the method in the debruijn graph code base. Just the base library for now

---
+++ common/target/debug/build/proc-macro2-f610bffc09b2a1cf/build_script_build-f610bffc09b2a1cf.d
@@ -0,0 +1,5 @@
+/home/joshfactorial/code/rusty-neat/common/target/debug/build/proc-macro2-f610bffc09b2a1cf/build_script_build-f610bffc09b2a1cf: /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
+
+/home/joshfactorial/code/rusty-neat/common/target/debug/build/proc-macro2-f610bffc09b2a1cf/build_script_build-f610bffc09b2a1cf.d: /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
+
+/home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs:
---
+++ common/target/debug/build/serde-c5d24cc150d41ca2/build_script_build-c5d24cc150d41ca2.d
@@ -0,0 +1,5 @@
+/home/joshfactorial/code/rusty-neat/common/target/debug/build/serde-c5d24cc150d41ca2/build_script_build-c5d24cc150d41ca2: /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.204/build.rs
+
+/home/joshfactorial/code/rusty-neat/common/target/debug/build/serde-c5d24cc150d41ca2/build_script_build-c5d24cc150d41ca2.d: /home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.204/build.rs
+
+/home/joshfactorial/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.204/build.rs:

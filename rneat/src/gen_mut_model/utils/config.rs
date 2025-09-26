049aa891a6d8d33d80d75cae0ed91a302562c1ae
Joshua Allen <joshfactorial@gmail.com>
2025-09-26 01:38:02 -0500

    Renamed module to gen_mut_model like in Python

---
+++ rneat/src/gen_mut_model/utils/config.rs
@@ -0,0 +1,137 @@
+// This is the run configuration for this particular run, which holds the parameters needed by the
+// various side functions. It is build with a ConfigurationBuilder, which can take either a
+// config yaml file or command line arguments and turn them into the configuration.
+use serde_yml::Value;
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::string::String;
+use std::fs;
+use common::{
+    file_tools::{
+        bed_reader::read_bed, 
+        vcf_tools::read_vcf
+    }, 
+    structs::{
+        bed_record::BedRecord, 
+        variants::Variant
+    }
+};
+use crate::gen_mut_model::errors::CreateMutationModelError;
+
+#[derive(Debug, Clone)]
+pub struct RunConfiguration {
+    // This struct holds all the parameters for this filter-reads run. It is built from user-supplied input
+    // in the form of a configuration yaml file
+    //
+    // bed_file: The path to the bed_file for the run.
+    // files_to_filter: The list of files to filter.
+    // filter_key: The key to add to the filtered file names so you know they have been filtered.
+    pub reference: PathBuf,
+    pub mutations: HashMap<String, Vec<Variant>>,
+    pub bed_table: HashMap<String, Vec<BedRecord>>,
+    pub output_file: PathBuf,
+    pub overwrite_output: bool,
+}
+
+impl RunConfiguration {
+    pub fn from(yml_file: &PathBuf) -> Result<Self, CreateMutationModelError> {
+        // Reads an input configuration file from yaml using the serde package. Then sets the
+        // parameters based on the inputs. A "." value means to use the default value.
+        //
+        // Opens file for reading
+        let f = fs::File::open(&yml_file);
+        let file = match f {
+            Ok(l) => l,
+            Err(error) => panic!(
+                "Problem reading the config file: {:?}. System error: {}",
+                &yml_file,
+                error,
+            ),
+        };
+        // Uses serde_yaml to read the file into a HashMap
+        let scrape_config: HashMap<String, Value> = serde_yml::from_reader(file)
+            .expect("Error reading yaml file!");
+        // Fill in the bed_file first, all hinges on that
+        let reference = PathBuf::from(scrape_config["reference"].as_str().unwrap());
+        if !reference.is_file() {
+            panic!("Invalid reference file {:?}", reference)
+        }
+        let vcf_file = PathBuf::from(scrape_config["vcf_file"].as_str().unwrap());
+        if !vcf_file.is_file() {
+            panic!("Invalid bed file {:?}", vcf_file)
+        }
+        let bed_file = PathBuf::from(scrape_config["bed_file"].as_str().unwrap());
+        if !bed_file.is_file() {
+            panic!("Invalid bed file {:?}", bed_file)
+        }
+        let overwrite_choice = scrape_config["overwrite_output"].as_bool().unwrap();
+        let overwrite_output = {
+            match overwrite_choice {
+                value => {
+                    if value == true {
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        };
+        let output_file = PathBuf::from(scrape_config["output_file"].as_str().unwrap());
+        if !overwrite_output {
+            if output_file.is_file() {
+                panic!("Attempting to overwrite an existing file {:?}", output_file)
+            }
+        }
+        // Generate the configuration from inputs
+        let bed_table = read_bed(&bed_file)
+            .expect("Error reading bed file!");
+        let mutations = read_vcf(vcf_file)?;
+        
+        Ok(RunConfiguration { 
+            reference,
+            mutations,
+            bed_table,
+            output_file,
+            overwrite_output,
+        })
+    }
+}
+
+pub fn create_map_item(
+    split_string: Vec<&str>, 
+    raw_string: &str,
+    length: usize,
+    filter_key: &str,
+) -> (PathBuf, PathBuf) {
+    let old_element = split_string[length-3];
+    let new_element = format!("{old_element}{filter_key}");
+    let mut output_name = String::new();
+    // stop one short of the end
+    for i in 0..length-1 {
+        if i == length-3 {
+            output_name.push_str(&new_element);
+            output_name.push('.');
+        } else {
+            output_name.push_str(split_string[i]);
+            output_name.push('.');
+        }
+    }
+    // End with the last extension with no traling dot.
+    output_name.push_str(split_string[length-1]);
+    let temp_path = PathBuf::from(raw_string);
+    if !temp_path.is_file() {
+        panic!("Input file not found! {:?}", temp_path)
+    }
+    (temp_path, PathBuf::from(output_name))
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn test_run_configuration() {
+        // TODO create test
+        assert!(true)
+    }
+}
---
+++ rneat/src/gen_mut_model/utils/runner.rs
@@ -0,0 +1,146 @@
+use std::{collections::HashMap, path::PathBuf};
+use itertools::Itertools;
+use log::*;
+
+use common::{
+    models::snp_trinuc_model::TrinucFrame, 
+    structs::{
+        bed_record::BedRecord, 
+        fasta_map::{FastaMap, RegionType, SequenceBlock}, 
+        nucleotides::Nucleotide, 
+        variants::{Variant, VariantType}
+    }
+};
+
+use crate::gen_mut_model::errors::CreateMutationModelError;
+
+pub fn runner(
+    fasta_map: FastaMap,
+    filtered_mutations: HashMap<String, Vec<Variant>>,
+    bed_table: HashMap<String, Vec<BedRecord>>,
+    output_file: &PathBuf,
+) -> Result<(), CreateMutationModelError> {
+    // This will generate a MutationModel and write it to the output file 
+    // provided.
+    // params:
+    //   fasta_map: The database for the fasta file, with links back to the 
+    //      original sequences
+    //   filtered_mutations: The mutations from the original VCF, after any 
+    //      appropriate bed filtering has been applied
+    //   bed_table: The hash map representing thi input bed
+    //   output_file: Where to write the output model
+    //
+    // This will store the count of trinucleotide combo A transitioning to combo B
+    // This is the raw data for the trinuc model.
+    let mut trinuc_transition_count: HashMap<(TrinucFrame, TrinucFrame), usize> = HashMap::new();
+    let mut snp_count = 0;
+    let mut snp_transition_count: HashMap<(Nucleotide, Nucleotide), usize> = HashMap::new();
+    let mut insertion_count: HashMap<usize, usize> = HashMap::new();
+    let mut deletion_count: HashMap<usize, usize> = HashMap::new();
+    let mut homozygous_count = 0;
+    let mut common_variants: Vec<(String, Variant, f64)> = Vec::new();
+    let trinuc_count = count_trinculeotides (
+        &fasta_map,
+        bed_table,
+    )?;
+    if trinuc_count.is_empty() {
+        error!("Trinucleotide counts were empty");
+        return Err(
+            CreateMutationModelError::TrinucCountError(
+                "Trinuc counts are empty. Unkown error".to_string()
+            )
+        )
+    }
+    let mut total_reflen = 0;
+    for contig in &fasta_map.contigs{
+        let mut non_n_region_count: usize = 0;
+        let countable_regions = contig.get_non_n_regions()?;
+        for region in countable_regions {
+            // I think the coordinates are forward on all regions
+            total_reflen += region.end - region.start;
+        }
+        let matching_variants = &filtered_mutations[&contig.name];
+        if matching_variants.is_empty() {
+            debug!("No variants found for {}", contig.name);
+            continue
+        }
+        for variant in matching_variants {
+            match variant.variant_type {
+                VariantType::SNP => {
+                    snp_count += 1;
+                    // Retrieve the trinucleotide sequence from the sequence block
+                    // If this is slow we may need to load the entire block and comb it.
+                    let trinuc_to_analyze = contig.get_block_subseq(variant.location-1, variant.location+2)?;
+                    // I'm curious to see what happens if the above is 4 bases long.
+                    // does impl From implement some error check under the hood?
+                    let ref_array: [&Nucleotide; 3] = trinuc_to_analyze.iter().collect_array().unwrap(); 
+                    let ref_frame = TrinucFrame::from(ref_array);
+                    let alt_array = [ref_array[0], &variant.alternate[0], ref_array[2]];
+                    let alt_frame = TrinucFrame::from(alt_array);
+                    if *ref_array[1] != variant.reference[0] {
+                        return Err(CreateMutationModelError::BaseMismatch(format!("{:?}", variant)))
+                    }
+                    *trinuc_transition_count.entry((ref_frame, alt_frame)).or_default() += 1;
+                    *snp_transition_count.entry((variant.reference[0], variant.alternate[0])).or_default() += 1;
+                },
+                VariantType::Insertion => {
+                    let variant_len = variant.alternate.len() - variant.reference.len();
+                    *insertion_count.entry(variant_len).or_default() += 1;
+                },
+                VariantType::Deletion => {
+                    let variant_len = variant.reference.len() - variant.alternate.len();
+                    *deletion_count.entry(variant_len).or_default() += 1;
+                },
+                _ => debug!("Unknown variant type, skipping for this analyis."),
+            }
+            let var_pop_freq = find_caf(&variant.alternate);
+
+        }
+    }
+    Ok(())
+}
+
+fn find_caf(alt: &Vec<Nucleotide>) -> Result<f64, CreateMutationModelError> {
+    // Python:
+    // info_split = [a.split('=') for a in candidate_field.split(';')]
+    // for item in info_split:
+    //     if item[0].upper() == 'CAF':
+    //         if ',' in item[1]:
+    //             return float(item[1].split(',')[1])
+    // return VCF_DEFAULT_POP_FREQ
+    todo!()
+}
+
+fn count_trinculeotides(
+    fasta_map: &FastaMap,
+    bed_table: HashMap<String, Vec<BedRecord>>,
+) -> Result<HashMap<TrinucFrame, usize>, CreateMutationModelError> {
+    // Counts the frequency of the various trinucleotide combinations in the dataset
+    // fasta_map - Database for reading back the sequenced data.
+    // bed_table - A table for the bed records
+
+    // HashMap of trinuce to count, e.g., "AAA": 12
+    let mut trinuc_count: HashMap<TrinucFrame, usize> = HashMap::new();
+    if !bed_table.is_empty() {
+        // There is actual bed input
+        info!("Counting trinucleotide combinations in bed regions");
+        for (chrom, bed_vec) in bed_table {
+            let contig_block = fasta_map.retrieve_contig(chrom.clone())
+                .expect(&format!("Error retrieving contig {}", chrom));
+            let regions_of_interest = contig_block.get_non_n_regions()
+                .expect("Error retrieving non-n regions!");
+            for region in regions_of_interest {
+                // iterate over the regions, grab trinucleotides, profit
+                // The first trincu is one base in. The last is one base from the end
+                for i in (region.start+1)..(region.end-1) {
+                    let trinuc = contig_block.get_block_subseq(i-1, i+1)?;
+                    let frame = TrinucFrame::from((trinuc[0], trinuc[1], trinuc[2]));
+                    *trinuc_count.entry(frame.to_owned()).or_default() += 1;
+                }
+            }
+        }
+    } else {
+        // No bed input
+    }
+    Ok(trinuc_count)
+}

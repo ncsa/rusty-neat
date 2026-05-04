4295378ac45c7e8bf7e776450d5739c0a2e61151
Joshua Allen <jallen17@illinois.edu>
2026-05-03 23:55:11 -0500

    Fixing typo in comment

--- common/src/models/fragment_length.rs
+++ common/src/models/fragment_length.rs
@@ -1,6 +1,6 @@
 //! In DNA sequencing, a fragment is a bit of DNA, roughly uniform in length that is sequenced
 //! by the machine. Sometimes these fragments have special molecules attached to the end for ID
-//! purposes. How this is done is a process called Chemistry Magic. For our purposes, we exect
+//! purposes. How this is done is a process called Chemistry Magic. For our purposes, we expect
 //! the data to be uniform enough that a mean and standard deviation will describe the set.
 
 use flate2::read::GzDecoder;
